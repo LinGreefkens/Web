6e4347678811d82f5b00f48debb4bfe39634b5cb
LinGreefkens <144111487+LinGreefkens@users.noreply.github.com>
2023-09-25 10:50:31 +0200

    Add files via upload

--- basiswebsite/scripts/script.js
+++ basiswebsite/scripts/script.js
@@ -1,2 +1,65 @@
-// JavaScript Document
 console.log("hi");
+
+
+
+
+
+
+// Selecteer knop hamburgermenu
+var hamburgerButton = document.querySelector('.hamburger-menu-button');
+// Selecteer het menu (de UL met alle li's)
+var menu = document.querySelector('.hamburger-menu');
+// selecteert het zoek icoontje in de footer
+var searchIcons = document.querySelectorAll('.search-icon');
+
+hamburgerButton.addEventListener('click', function () {
+  menu.classList.toggle('active');
+
+  // If menu is geopend dan is het kruisje actief + zoek icoontje weg
+  if (menu.classList.contains('active')) {
+    hamburgerButton.src = 'images/cross.svg';
+    searchIcons.forEach(icon => icon.style.display = 'none');
+    // Else menu is gesloten dan is het hamburger menu actief + zoek icoontje zichtbaar
+  } else {
+    hamburgerButton.src = 'images/hamburgermenu_button.svg';
+    searchIcons.forEach(icon => icon.style.display = 'inline');
+  }
+});
+
+console.log(window.location.pathname);
+
+if (window.location.pathname === '/index.html') {
+  // /* Selecteer knop justAnnounced */
+var buttonJustAnnounced = document.querySelector(".button_justAnnounced");
+// /* Selecteer articles van justAnnounced */
+var articlesJustAnnounced = document.querySelector(".Articles_justAnnounced");
+
+// /* Selecteer knop nextUp */
+var buttonNextUp = document.querySelector(".button_nextUp");
+// /* Selecteer articles nextUp */
+var articlesNextUp = document.querySelector(".Articles_nextUp");
+
+// Functie om knopkleuren bij te werken
+function updateButtonColors(selectedButton, otherButton) {
+    selectedButton.style.color = "black";
+    otherButton.style.color = "grey";
+}
+
+// Standaard staat de "Just Announced" knop geselecteerd
+articlesJustAnnounced.classList.remove("hidden");
+articlesNextUp.classList.add("hidden");
+updateButtonColors(buttonJustAnnounced, buttonNextUp);
+// Klik op just announced button
+buttonJustAnnounced.addEventListener("click", function () {
+  articlesJustAnnounced.classList.remove("hidden");
+  articlesNextUp.classList.add("hidden");
+  updateButtonColors(buttonJustAnnounced, buttonNextUp);
+});
+
+// Klik op next up button
+buttonNextUp.addEventListener("click", function () {
+  articlesJustAnnounced.classList.add("hidden");
+  articlesNextUp.classList.remove("hidden");
+  updateButtonColors(buttonNextUp, buttonJustAnnounced);
+}); 
+}
